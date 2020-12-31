4f2797130269ead749fa59b0fd0b1ba77e547624
hjs6877 <hjs6877@gmail.com>
2020-12-31 11:26:46 +0900

    Solve Chapter04Problem01

---
+++ src/main/java/com/itvillage/book/this_is_coding_test/part2/Chapter04Problem01.java
@@ -0,0 +1,26 @@
+package com.itvillage.book.this_is_coding_test.part2;
+
+import java.util.Scanner;
+
+public class Chapter04Problem01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String pos = scanner.nextLine();
+        int col = pos.charAt(0) - 'a' + 1;
+        int row = Character.getNumericValue(pos.charAt(1));
+
+        int[][] directions = {{-2, -1}, {-2, 1}, {2, -1}, {2, 1}, {-1, -2}, {1, -2}, {-1, 2}, {1, 2}};
+
+        int count = 0;
+        for (int[] direction : directions) {
+            int nextRow = row + direction[0];
+            int nextCol = col + direction[1];
+
+            if (nextRow >= 1 && nextRow <= 8 && nextCol >= 1 && nextRow <= 8) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}

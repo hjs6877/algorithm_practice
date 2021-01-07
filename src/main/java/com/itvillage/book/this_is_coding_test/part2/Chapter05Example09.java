5f2a451a4df4731f55042f98a5af8b1f21762f15
hjs6877 <hjs6877@gmail.com>
2021-01-07 23:06:34 +0900

    Implementing BFS Example

---
+++ src/main/java/com/itvillage/book/this_is_coding_test/part2/Chapter05Example09.java
@@ -0,0 +1,47 @@
+package com.itvillage.book.this_is_coding_test.part2;
+
+import java.util.*;
+
+/**
+ * BFS 예제
+ */
+public class Chapter05Example09 {
+    public static List<List<Integer>> graph = new ArrayList<>();
+    public static boolean[] visited = new boolean[9];
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 9; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        graph.get(1).add(2);
+        graph.get(1).add(3);
+        graph.get(1).add(8);
+
+        graph.get(2).add(1);
+        graph.get(2).add(7);
+
+        graph.get(3).add(4);
+        graph.get(3).add(5);
+
+        graph.get(4).add(3);
+        graph.get(4).add(5);
+
+        graph.get(5).add(3);
+        graph.get(5).add(4);
+
+        graph.get(6).add(7);
+
+        graph.get(7).add(6);
+        graph.get(7).add(8);
+
+        graph.get(8).add(1);
+        graph.get(8).add(7);
+
+        bfs(1);
+    }
+
+    private static void bfs(int start) {
+
+    }
+}

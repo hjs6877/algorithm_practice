05f262ecbb5caedabedc858e35f232fdf447cb27
hjs6877 <hjs6877@gmail.com>
2021-01-12 22:42:28 +0900

    Solve Problem BFS

---
+++ src/main/java/com/itvillage/book/this_is_coding_test/part2/Chapter05Problem04.java
@@ -0,0 +1,73 @@
+package com.itvillage.book.this_is_coding_test.part2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Chapter05Problem04 {
+    public static int n,m;
+    public static int[][] graph;
+    public static int[] dx = {-1, 1, 0, 0};
+    public static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        m = scanner.nextInt();
+        scanner.nextLine();
+        graph = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String str = scanner.nextLine();
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = str.charAt(i) - '0';
+            }
+        }
+
+        int result = bfs(0, 0);
+    }
+
+    private static int bfs(int x, int y) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(x, y));
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            x = node.getX();
+            y = node.getY();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if(graph[nx][ny] == 0) continue;
+
+                if (graph[nx][ny] == 1) {
+                    graph[nx][ny] = graph[x][y] + 1;
+                    queue.offer(new Node(nx, ny));
+                }
+            }
+        }
+
+        return graph[n - 1][m - 1];
+    }
+}
+
+class Node {
+    private int x;
+    private int y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}

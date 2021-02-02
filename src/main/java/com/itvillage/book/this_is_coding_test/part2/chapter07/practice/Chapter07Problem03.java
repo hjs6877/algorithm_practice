507e76d239e769aa7418910d7dcff3b5dda56cfc
hjs6877 <hjs6877@gmail.com>
2021-02-02 23:33:08 +0900

    Solving Chapter07Problem03

---
+++ src/main/java/com/itvillage/book/this_is_coding_test/part2/chapter07/practice/Chapter07Problem03.java
@@ -0,0 +1,18 @@
+package com.itvillage.book.this_is_coding_test.part2.chapter07.practice;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Chapter07Problem03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+    }
+}

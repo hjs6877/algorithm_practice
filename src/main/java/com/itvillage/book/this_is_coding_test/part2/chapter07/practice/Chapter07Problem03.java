a7393973f50683fc2c842fb90ef430e42f79888b
hjs6877 <hjs6877@gmail.com>
2021-02-03 22:13:26 +0900

    Solve Chapter07Problem03

--- src/main/java/com/itvillage/book/this_is_coding_test/part2/chapter07/practice/Chapter07Problem03.java
+++ src/main/java/com/itvillage/book/this_is_coding_test/part2/chapter07/practice/Chapter07Problem03.java
@@ -1,7 +1,5 @@
 package com.itvillage.book.this_is_coding_test.part2.chapter07.practice;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Chapter07Problem03 {
@@ -9,10 +7,38 @@ public class Chapter07Problem03 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        Integer[] arr = new Integer[n];
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > end) {
+                end = arr[i];
+            }
+        }
+
+        int result = 0;
+        while (start <= end) {
+            int total = 0;
+            int mid = (start + end) / 2;
+
+            for (int length : arr) {
+                if (length > mid) {
+                    total += length - mid;
+                }
+            }
+
+            if (total > m) {
+                start = mid +1;
+            } else {
+                end = mid - 1;
+            }
+            result = mid;
+        }
+
+        System.out.print(result);
     }
 }
---
+++ src/main/java/com/itvillage/book/this_is_coding_test/part2/chapter07/solve/Chapter07Problem03.java
@@ -0,0 +1,9 @@
+package com.itvillage.book.this_is_coding_test.part2.chapter07.solve;
+
+import java.util.Scanner;
+
+public class Chapter07Problem03 {
+    public static void main(String[] args) {
+
+    }
+}

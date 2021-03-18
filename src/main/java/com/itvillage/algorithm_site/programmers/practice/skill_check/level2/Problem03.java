3cc75468aa8cfecc9d8ac1aa394a64e28a4d5da1
hjs6877 <hjs6877@gmail.com>
2021-03-18 18:43:20 +0900

    Soleve Problem03

---
+++ src/main/java/com/itvillage/algorithm_site/programmers/practice/skill_check/level2/Problem03.java
@@ -0,0 +1,30 @@
+package com.itvillage.algorithm_site.programmers.practice.skill_check.level2;
+
+import java.util.Scanner;
+
+/**
+ * 점프 이외의 이동시 배터리 소모량 문제
+ */
+public class Problem03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int result = solution(n);
+
+        System.out.println(result);
+    }
+    public static int solution(int n) {
+        int count = 0;
+        if (n % 2 == 0) {
+            while (n > 1) {
+                if (n % 2 != 0) {
+                    count = count + (n % 2);
+                }
+                n /= 2;
+            }
+        } else {
+            count = n % 2;
+        }
+        return count + 1;
+    }
+}

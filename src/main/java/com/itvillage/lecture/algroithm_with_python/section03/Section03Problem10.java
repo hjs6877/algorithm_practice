e8470661b3d838698a67fff65b8e0a1e4d4a877a
hjs6877 <hjs6877@gmail.com>
2021-03-24 17:33:34 +0900

    Solve Section03 > Problem10

---
+++ src/main/java/com/itvillage/lecture/algroithm_with_python/section03/Section03Problem10.java
@@ -0,0 +1,67 @@
+package com.itvillage.lecture.algroithm_with_python.section03;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * 스도쿠 검사
+ */
+public class Section03Problem10 {
+    public static int[] count = new int[10];
+    public static boolean isRight = true;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] arr = new int[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+
+
+        isRight = isRight(arr, 9, "row");
+
+        if (isRight) {
+            isRight = isRight(arr, 9, "col") ;
+        }
+        int n = 3;
+
+        while (isRight && n <= 9) {
+            Arrays.fill(count, 0);
+            isRight = isRight(arr, n, "row");
+            if (!isRight) {
+                break;
+            }
+            n += 3;
+        }
+
+        if (isRight) {
+            System.out.print("YES");
+        } else {
+            System.out.print("NO");
+        }
+    }
+
+    private static boolean isRight(int[][] arr, int n, String type) {
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(count, 0);
+            for (int j = 0; j < n; j++) {
+                int idx = arr[i][j];
+                if (type.equals("col")) {
+                    idx = arr[j][i];
+                }
+                if (count[idx] == 0) {
+                    count[idx] = 1;
+                } else {
+                    isRight = false;
+                    break;
+                }
+            }
+            if (!isRight) {
+                break;
+            }
+        }
+        return isRight;
+    }
+}

2a4f992b6543d4e09f30b1832169adf43b210b65
hjs6877 <hjs6877@gmail.com>
2021-04-22 09:53:46 +0900

    Solve Section06 > Problem1302

--- src/main/java/com/itvillage/lecture/algroithm_with_python/section06/Section06Problem1302.java
+++ src/main/java/com/itvillage/lecture/algroithm_with_python/section06/Section06Problem1302.java
@@ -5,10 +5,10 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
- * 경로 탐색(그래프 DFS)
+ * 경로 탐색(그래프 DFS) : 인접 행렬 사용
  */
-public class Section06Problem13 {
-    static List<List<Integer>> graph = new ArrayList<>();
+public class Section06Problem1302 {
+    static int[][] metrics;
     static int n;
     static int result;
     static boolean[] visited;
@@ -17,16 +17,12 @@ public class Section06Problem13 {
         n = scanner.nextInt();
         int m = scanner.nextInt();
         visited = new boolean[n + 1];
-
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
+        metrics = new int[n + 1][n + 1];
 
         for (int i = 0; i < m; i++) {
-            graph.get(scanner.nextInt()).add(scanner.nextInt());
+            metrics[scanner.nextInt()][scanner.nextInt()] = 1;
         }
 
-        
         dfs(1);
         System.out.print(result);
     }
@@ -34,15 +30,17 @@ public class Section06Problem13 {
     private static void dfs(int v) {
         visited[v] = true;
 
-        for (int i = 0; i < graph.get(v).size(); i++) {
-            int y = graph.get(v).get(i);
-
-            if (y == n) {
-                result++;
-                return;
-            } else if(!visited[y]){
-                dfs(y);
-                visited[y] = false;
+        if (v == n) {
+            result++;
+            return;
+        } else {
+            for (int i = 1; i < n + 1; i++) {
+                if (metrics[v][i] == 1) {
+                    if (!visited[i]) {
+                        dfs(i);
+                        visited[i] = false;
+                    }
+                }
             }
         }
     }

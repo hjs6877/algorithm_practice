a907dcb5112e0c76323f944c96380bc19514b797
hjs6877 <hjs6877@gmail.com>
2021-05-04 17:11:15 +0900

    Solve Section07 > Problem02

--- src/main/java/com/itvillage/lecture/algroithm_with_python/section07/Section07Problem02.java
+++ src/main/java/com/itvillage/lecture/algroithm_with_python/section07/Section07Problem02.java
@@ -9,33 +9,34 @@ import java.util.Scanner;
 public class Section07Problem02 {
     static int n;
     static int[][] tp;
+    static int maxPrice;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         n = scanner.nextInt();
-        tp = new int[n][2];
+        tp = new int[n+1][2];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             tp[i][0] = scanner.nextInt();
             tp[i][1] = scanner.nextInt();
         }
 
-        int maxPrice = Integer.MIN_VALUE;
+        maxPrice = Integer.MIN_VALUE;
 
-        dfs(0, 0, 0);
+        dfs(1, 0);
+
+        System.out.println(maxPrice);
     }
 
-    private static void dfs(int level, int t, int p) {
-        if (t > n) {
-            return;
+    private static void dfs(int level, int sum) {
+        if (level == n + 1) {
+            maxPrice = Math.max(maxPrice, sum);
         }
-
-        if (t == n) {
-
-        } else {
-            for (int i = 0; i < n; i++) {
-                dfs(level + 1, t + tp[i][0], p + tp[i][1]);
-                dfs(level + 1, t, p);
+        else {
+            if (level + tp[level][0] <= n + 1) {
+                dfs(level + tp[level][0], sum + tp[level][1]);
+            } else {
+                dfs(level + 1, sum);
             }
         }
 

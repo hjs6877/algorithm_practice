c441cbb0e688b4036b2798d527d657432b91c815
hjs6877 <hjs6877@gmail.com>
2021-05-12 22:57:53 +0900

    Complete Section06 > Problem04 > Practice01

---
+++ src/main/java/com/itvillage/lecture/algroithm_with_python/section06/Section06Problem03Practice01.java
@@ -0,0 +1,35 @@
+package com.itvillage.lecture.algroithm_with_python.section06;
+
+import java.util.Scanner;
+
+/**
+ * 부분 집합 구하기(DFS) Practice01
+ */
+public class Section06Problem03Practice01 {
+    static boolean[] used;
+    static int n;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        used = new boolean[n + 1];
+
+        dfs(1);
+    }
+
+    private static void dfs(int v) {
+        if (v == n + 1) {
+            for (int i = 0; i < used.length; i++) {
+                if (used[i]) {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println();
+        } else {
+            used[v] = true;
+            dfs(v + 1);
+
+            used[v] = false;
+            dfs(v + 1);
+        }
+    }
+}
---
+++ src/main/java/com/itvillage/lecture/algroithm_with_python/section06/Section06Problem04Practice01.java
@@ -0,0 +1,38 @@
+package com.itvillage.lecture.algroithm_with_python.section06;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * 합이 같은 부분 집합 Practice01
+ */
+public class Section06Problem04Practice01 {
+    static int n;
+    static int[] arr;
+    static int total;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        total = Arrays.stream(arr).sum();
+        dfs(0, 0);
+    }
+
+    private static void dfs(int level, int sum) {
+        if (sum > total / 2) {
+            return;
+        }
+        if (level == n) {
+            if (total - sum == sum) {
+                System.out.print("YES");
+                System.exit(0);
+            }
+        } else {
+            dfs(level + 1, sum + arr[level]);
+            dfs(level + 1, sum);
+        }
+    }
+}

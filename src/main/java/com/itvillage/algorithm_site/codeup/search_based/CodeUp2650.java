b7707daf27f96442d92ce8eefb9b65af3c9fedc7
hjs6877 <hjs6877@gmail.com>
2021-06-17 23:55:52 +0900

    Solve CodeUp2650

--- src/main/java/com/itvillage/algorithm_site/codeup/search_based/CodeUp2650.java
+++ src/main/java/com/itvillage/algorithm_site/codeup/search_based/CodeUp2650.java
@@ -1,4 +1,31 @@
 package com.itvillage.algorithm_site.codeup.search_based;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class CodeUp2650 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[3];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int gcd = gcd(arr[0], arr[1]);
+        int result2 = gcd(arr[2], gcd);
+
+        System.out.print(result2);
+    }
+
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
 }

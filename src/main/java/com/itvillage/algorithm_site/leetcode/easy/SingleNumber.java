1a955f564293a9e98f6d01bfeeaf28438bebd75f
hjs6877 <hjs6877@gmail.com>
2021-07-01 22:37:09 +0900

    Solve SingleNumber

---
+++ src/main/java/com/itvillage/algorithm_site/leetcode/easy/SingleNumber.java
@@ -0,0 +1,38 @@
+package com.itvillage.algorithm_site.leetcode.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class SingleNumber {
+    public static void main(String[] args) {
+        int[] arr = {-1};
+        int result = singleNumber(arr);
+        System.out.print(result);
+    }
+    public static int singleNumber(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            if (map.get(n) != null) {
+                map.put(n, map.get(n) + 1);
+            } else {
+                map.put(n, 1);
+            }
+        }
+
+        int result = 0;
+        for (Integer key : map.keySet()) {
+            if (map.get(key) == 1) {
+                result = key;
+                break;
+            }
+        }
+
+        return result;
+    }
+}

b0ecc817b5617524fbf08dc872b88a54462242e9
hjs6877 <hjs6877@gmail.com>
2021-07-12 22:19:50 +0900

    Solve RichestCustomerWealth

---
+++ src/main/java/com/itvillage/algorithm_site/leetcode/easy/RichestCustomerWealth.java
@@ -0,0 +1,21 @@
+package com.itvillage.algorithm_site.leetcode.easy;
+
+public class RichestCustomerWealth {
+    public static void main(String[] args) {
+        int[][] accounts = {{1,5},{7,3},{3,5}};
+        int result = maximumWealth(accounts);
+        System.out.print(result);
+    }
+
+    public static int maximumWealth(int[][] accounts) {
+        int maxWealth = Integer.MIN_VALUE;
+        for (int[] customers : accounts) {
+            int sum = 0;
+            for (int money : customers) {
+                sum += money;
+            }
+            maxWealth = Math.max(maxWealth, sum);
+        }
+        return maxWealth;
+    }
+}

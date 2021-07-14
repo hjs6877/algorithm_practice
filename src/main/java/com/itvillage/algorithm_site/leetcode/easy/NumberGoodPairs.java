d0bd6979330dc16a4e44a45991e96eeb1b85897e
hjs6877 <hjs6877@gmail.com>
2021-07-14 14:16:28 +0900

    Solve NumberGoodPairs

---
+++ src/main/java/com/itvillage/algorithm_site/leetcode/easy/NumberGoodPairs.java
@@ -0,0 +1,22 @@
+package com.itvillage.algorithm_site.leetcode.easy;
+
+public class NumberGoodPairs {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1,1,3};
+        int result = numIdenticalPairs(nums);
+
+        System.out.print(result);
+    }
+
+    public static int numIdenticalPairs(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+}

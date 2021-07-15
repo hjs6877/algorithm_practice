8ed9fbf4fa0e5a65ed23300a3e43ef0339ed9685
hjs6877 <hjs6877@gmail.com>
2021-07-15 22:51:49 +0900

    Solve ShuffleString

---
+++ src/main/java/com/itvillage/algorithm_site/leetcode/easy/ShuffleString.java
@@ -0,0 +1,20 @@
+package com.itvillage.algorithm_site.leetcode.easy;
+
+public class ShuffleString {
+    public static void main(String[] args) {
+        String s = "codeleet";
+        int[] indices = {4,5,6,7,0,2,1,3};
+
+        String result = restoreString(s, indices);
+        System.out.print(result);
+    }
+
+    public static String restoreString(String s, int[] indices) {
+        char[] chs = s.toCharArray();
+        char[] result = new char[chs.length];
+        for (int i = 0; i < indices.length; i++) {
+            result[indices[i]] = chs[i];
+        }
+        return new String(result);
+    }
+}

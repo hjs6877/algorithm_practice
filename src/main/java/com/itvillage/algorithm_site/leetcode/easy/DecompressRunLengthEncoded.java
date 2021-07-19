226d1085018c189ca5e12c0b9de439a762fec1c2
hjs6877 <hjs6877@gmail.com>
2021-07-19 15:54:12 +0900

    Solve DecompressRunLengthEncoded

---
+++ src/main/java/com/itvillage/algorithm_site/leetcode/easy/DecompressRunLengthEncoded.java
@@ -0,0 +1,34 @@
+package com.itvillage.algorithm_site.leetcode.easy;
+
+import java.util.Arrays;
+
+public class DecompressRunLengthEncoded {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        int[] result = decompressRLElist(nums);
+
+        for (int n : result) {
+            System.out.print(n + " ");
+        }
+    }
+
+    public static int[] decompressRLElist(int[] nums) {
+        int length = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                length += nums[i];
+            }
+        }
+        int[] result = new int[length];
+        int k = 0;
+        for (int i = 0; i < nums.length / 2; i++) {
+            int freq = nums[2 * i];
+            int val = nums[2 * i + 1];
+            for (int j = 0; j < freq; j++) {
+                result[k++] = val;
+            }
+        }
+
+        return result;
+    }
+}

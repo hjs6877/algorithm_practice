4b187a8fcb45b1bb6bc263efb27b060a11e2aabe
hjs6877 <hjs6877@gmail.com>
2021-07-19 14:27:21 +0900

    Solve NumberStepReduce

---
+++ src/main/java/com/itvillage/algorithm_site/leetcode/easy/NumberStepReduce.java
@@ -0,0 +1,23 @@
+package com.itvillage.algorithm_site.leetcode.easy;
+
+public class NumberStepReduce {
+    public static void main(String[] args) {
+        int num = 14;
+        int result = numberOfSteps(num);
+
+        System.out.print(result);
+    }
+
+    public static int numberOfSteps(int num) {
+        int count = 0;
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else {
+                num = num -1;
+            }
+            count++;
+        }
+        return count;
+    }
+}

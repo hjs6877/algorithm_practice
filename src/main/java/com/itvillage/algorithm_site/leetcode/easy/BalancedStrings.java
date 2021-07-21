8adec653f3f815f7b738b3fcff56ef4643c2a402
hjs6877 <hjs6877@gmail.com>
2021-07-21 13:51:25 +0900

    Solve BalancedStrings

---
+++ src/main/java/com/itvillage/algorithm_site/leetcode/easy/BalancedStrings.java
@@ -0,0 +1,31 @@
+package com.itvillage.algorithm_site.leetcode.easy;
+
+public class BalancedStrings {
+    public static void main(String[] args) {
+        String s = "RLRRRLLRLL";
+        int result = balancedStringSplit(s);
+
+        System.out.print(result);
+    }
+
+    public static int balancedStringSplit(String s) {
+        int lCount = 0;
+        int rCount = 0;
+        int result = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == 'R') {
+                rCount++;
+            } else {
+                lCount++;
+            }
+
+            if (lCount == rCount) {
+                result++;
+                lCount = 0;
+                rCount = 0;
+            }
+        }
+
+        return result;
+    }
+}

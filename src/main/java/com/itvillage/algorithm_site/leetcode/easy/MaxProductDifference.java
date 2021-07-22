840b99c509f014eb3e046d62b2c24f41135b9a3b
hjs6877 <hjs6877@gmail.com>
2021-07-22 14:09:22 +0900

    Solve MaxProductDifference

---
+++ src/main/java/com/itvillage/algorithm_site/leetcode/easy/MaxProductDifference.java
@@ -0,0 +1,17 @@
+package com.itvillage.algorithm_site.leetcode.easy;
+
+import java.util.Arrays;
+
+public class MaxProductDifference {
+    public static void main(String[] args) {
+        int[] nums = {4,2,5,9,7,4,8};
+        int result = maxProductDifference(nums);
+        System.out.print(result);
+    }
+
+    public static int maxProductDifference(int[] nums) {
+        Arrays.sort(nums);
+
+        return nums[nums.length - 1] * nums[nums.length - 2] - nums[0] * nums[1];
+    }
+}

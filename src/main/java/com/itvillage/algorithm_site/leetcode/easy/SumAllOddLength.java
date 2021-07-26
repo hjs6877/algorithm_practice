1a341578d962d53923396fce2b6dffa8c89f7df9
hjs6877 <hjs6877@gmail.com>
2021-07-26 22:36:32 +0900

    Solve SumAllOddLength

---
+++ src/main/java/com/itvillage/algorithm_site/leetcode/easy/SumAllOddLength.java
@@ -0,0 +1,23 @@
+package com.itvillage.algorithm_site.leetcode.easy;
+
+import java.util.Arrays;
+
+public class SumAllOddLength {
+    public static void main(String[] args) {
+        int[] arr = {1,4,2,5,3};
+        int result = sumOddLengthSubarrays(arr);
+
+        System.out.print(result);
+    }
+
+    public static int sumOddLengthSubarrays(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j = j + 2) {
+                int[] temp = Arrays.copyOfRange(arr, i, j + 1);
+                sum += Arrays.stream(temp).sum();
+            }
+        }
+        return sum;
+    }
+}

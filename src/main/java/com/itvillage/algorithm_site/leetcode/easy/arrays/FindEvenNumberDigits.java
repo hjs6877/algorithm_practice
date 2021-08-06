fbc6c85e64a12b3a7649cacf3b9525196a96b41d
hjs6877 <hjs6877@gmail.com>
2021-08-06 13:42:14 +0900

    Solve FindEvenNumberDigits

---
+++ src/main/java/com/itvillage/algorithm_site/leetcode/easy/arrays/FindEvenNumberDigits.java
@@ -0,0 +1,21 @@
+package com.itvillage.algorithm_site.leetcode.easy.arrays;
+
+public class FindEvenNumberDigits {
+    public static void main(String[] args) {
+        int[] nums = {555,901,482,1771};
+        int result = findNumbers(nums);
+
+        System.out.println(result);
+    }
+
+    public static int findNumbers(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if (String.valueOf(num).length() % 2 == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}

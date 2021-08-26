f0aefdb7c7a372a35dbec80725acd6214c1125e6
hjs6877 <hjs6877@gmail.com>
2021-08-26 22:43:45 +0900

    Solve SortArrayByParity

---
+++ src/main/java/com/itvillage/algorithm_site/leetcode/easy/arrays/SortArrayByParity.java
@@ -0,0 +1,27 @@
+package com.itvillage.algorithm_site.leetcode.easy.arrays;
+
+public class SortArrayByParity {
+    public static void main(String[] args) {
+        int[] nums = {3,1,2,4};
+        int[] result = sortArrayByParity(nums);
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int[] sortArrayByParity(int[] nums) {
+        int[] sorted = new int[nums.length];
+
+        for (int i = 0, j = nums.length - 1, k = 0; k < nums.length; k++) {
+            if (nums[k] % 2 == 0) {
+                sorted[i] = nums[k];
+                i++;
+            } else {
+                sorted[j] = nums[k];
+                j--;
+            }
+        }
+        return sorted;
+    }
+}

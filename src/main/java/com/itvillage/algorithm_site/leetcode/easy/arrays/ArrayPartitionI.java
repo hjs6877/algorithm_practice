c676c81f9b4cef1164b6b1eaf742260f57f0936d
hjs6877 <hjs6877@gmail.com>
2021-09-15 22:25:30 +0900

    Solve ArrayPartitionI

---
+++ src/main/java/com/itvillage/algorithm_site/leetcode/easy/arrays/ArrayPartitionI.java
@@ -0,0 +1,21 @@
+package com.itvillage.algorithm_site.leetcode.easy.arrays;
+
+import java.util.Arrays;
+
+public class ArrayPartitionI {
+    public static void main(String[] args) {
+        int[] nums= {6,2,6,5,1,2};
+        int result = arrayPairSum(nums);
+
+        System.out.print(result);
+    }
+
+    public static int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i = i + 2) {
+            sum += Math.min(nums[i], nums[i + 1]);
+        }
+        return sum;
+    }
+}

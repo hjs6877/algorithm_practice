e486a4564a8c205c63c1e3fd6329136084b20b3e
hjs6877 <hjs6877@gmail.com>
2021-10-20 23:14:18 +0900

    Solve Chapter11Problem03 for this is coding test

--- src/main/java/com/itvillage/book/this_is_coding_test/part3/chapter11_greedy/solve/Chapter11Problem02.java
+++ src/main/java/com/itvillage/book/this_is_coding_test/part3/chapter11_greedy/solve/Chapter11Problem02.java
@@ -2,6 +2,9 @@ package com.itvillage.book.this_is_coding_test.part3.chapter11_greedy.solve;
 
 import java.util.Scanner;
 
+/**
+ * 곱하기 혹은 더하기
+ */
 public class Chapter11Problem02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -10,13 +13,10 @@ public class Chapter11Problem02 {
         int sum = 0;
         for (char c : s.toCharArray()) {
             int num = Character.getNumericValue(c);
-            if (num == 0 || num == 1) {
-                sum = sum + num;
+            if (num <= 1 || sum <= 1) {
+                sum += num;
             } else {
-                if (sum == 0) {
-                    sum += 1;
-                }
-                sum = sum * num;
+                sum *= num;
             }
         }
 
---
+++ src/main/java/com/itvillage/book/this_is_coding_test/part3/chapter11_greedy/solve/Chapter11Problem03.java
@@ -0,0 +1,22 @@
+package com.itvillage.book.this_is_coding_test.part3.chapter11_greedy.solve;
+
+public class Chapter11Problem03 {
+    public static void main(String[] args) {
+        String s = "0001100";
+        int zeroCnt = 0;
+        int oneCnt = 0;
+
+        char now = s.charAt(0);
+        for (int i = 0; i < s.length() - 1; i++) {
+            char next = s.charAt(i + 1);
+            if (now != next) {
+                if(next == '0') oneCnt++;
+                if(next == '1') zeroCnt++;
+                now = next;
+            }
+        }
+        if(now == '0') zeroCnt++;
+        if(now == '1') oneCnt++;
+        System.out.print(Math.min(zeroCnt, oneCnt));
+    }
+}
